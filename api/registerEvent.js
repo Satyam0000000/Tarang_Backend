57ac0ef50fafab84ffb9d52875521e65b712face
Satyam0000000 <satyamgoswami613@gmail.com>
2026-01-04 13:23:54 +0530

    addeds cors to login and registerevent also

--- api/registerEvent.js
+++ api/registerEvent.js
@@ -1,7 +1,27 @@
 import connectDB from "../utils/connectDB";
 import EventRegistration from "../models/EventRegistration"
+import cors from "cors"
+
+const corsMiddleware = cors({
+    origin: "https://tarang-frontend.vercel.app",
+    methods: ["POST"],
+    credentials: true,
+});
+
+//middleware helper
+function runMiddleware(req,res,fn){
+    return new Promise((resolve, reject) => {
+        fn(req,res, (result)=> {
+            if (result instanceof Error) return reject(result);
+            return resolve(result);
+        })
+    })
+}
 
 export default async function handler(req,res) {
+
+    await runMiddleware(req,res,corsMiddleware);
+
 if(req.method !== "POST")
     return res.status(405).json({message: "Method not allowed"});
 await connectDB();
