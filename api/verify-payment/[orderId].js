162156307b8000ea0e6236dc086e085a397a928e
Satyam0000000 <satyamgoswami613@gmail.com>
2026-01-07 17:55:55 +0530

    add cors in createorder and orderid

--- api/verify-payment/[orderId].js
+++ api/verify-payment/[orderId].js
@@ -1,5 +1,30 @@
 import axios from "axios";
 
+const allowedOrigins = [
+    "https://tarang-frontend.vercel.app",
+  "https://www.tarangclub.online",
+];
+const corsMiddleware = cors({
+    origin: function (origin, callback){
+        if(!origin || allowedOrigins.includes(origin)){
+            callback(null, true);
+        }else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: ["POST","OPTIONS"],
+    credentials: true,
+});
+//middleware helper
+function runMiddleware(req,res,fn){
+    return new Promise((resolve, reject) => {
+        fn(req,res, (result)=> {
+            if (result instanceof Error) return reject(result);
+            return resolve(result);
+        })
+    })
+}
+
 export default async function handler(req, res) {
   const { orderId } = req.query;
 
