0b08b276eada946b2e09f778bd3fb86c1385bb0d
Satyam0000000 <satyamgoswami613@gmail.com>
2026-01-16 10:55:13 +0530

    user model fixed

--- models/User.js
+++ models/User.js
@@ -2,13 +2,39 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    fullName: String,
-    email: { type: String, unique: true },
-    productId: String,
-    examName: String,
-    price: Number,
-    orderId: String,
-    password: String, // if login uses password
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    emailOTP: {
+      type: String,
+      default: null,
+    },
+
+    otpExpiry: {
+      type: Date,
+      default: null,
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true }
 );
